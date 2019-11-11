ea6f14a93e262e45a40274f5f3b57a5e1dac2c22
Angel Corredor <adcorredorm@unal.edu.co>
2019-11-11 15:35:04 -0500

    Calculated Features: > Total Time Spent > Total Questions Answered > Total Helps Used > Average Time Spent > Percentile by Total Time > Percentile by Average Time

---
+++ features.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from datetime import datetime
+from truncate import read_time
+
+data = pd.read_csv('processed_data/data_a_train_5m.csv')
+ids = data.STUDENTID.unique()
+
+headers = ['ID', 'Time Spent', 'Total Questions', 'Total Helps', 'Average Time Spent']
+result = []
+
+for _id in ids:
+    query = 'STUDENTID == {}'.format(_id)
+    raw = data.query(query)
+
+    questions = raw[raw['AccessionNumber'].str.startswith('VH') == True].AccessionNumber.unique()
+    helps = raw[raw['AccessionNumber'].str.startswith('HELP') == True].AccessionNumber.unique()
+
+    time_spent = read_time(raw.iloc[-1][6]) - read_time(raw.iloc[0][6])
+    total_questions = len(questions)
+    total_helps = len(helps)
+    average = time_spent / total_questions
+
+    result.append([
+        _id, str(time_spent), total_questions, total_helps, str(average)
+    ])
+
+
+df = pd.DataFrame(result, columns=headers)
+df['Percentile Total Time Rank'] = df['Time Spent'].rank(pct=True)
+df['Percentile Average Time Rank'] = df['Average Time Spent'].rank(pct=True)
+df.to_csv('processed_data/features_5m.csv')
